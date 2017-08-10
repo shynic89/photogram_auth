cf88659fa09387e0cfe04816160e55853f63488d
Nick Su <hsu0@chicagobooth.edu>
2017-08-10 00:41:07 +0000

    test 1

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,12 @@
+class UsersController < ApplicationController
+# skip_before_action :authenticate_user!, :only => [:index]
+def index
+@user = User.all
+end
+
+def show
+@id = params[:id]
+@user = User.find(@id)
+end
+    
+end
